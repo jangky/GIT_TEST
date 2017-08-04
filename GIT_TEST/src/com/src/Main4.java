96c39e4d246373d08a0e5a5ac61805908ff2fb44
jangky <jangky@wesome.co.kr>
2017-08-04 11:11:53 +0900

    modified file [issue53]

--- GIT_TEST/src/com/src/Main4.java
+++ GIT_TEST/src/com/src/Main4.java
@@ -5,6 +5,7 @@ public class Main4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new brench");;
+		System.out.println("new brench retry");;
 	}
 
 }
